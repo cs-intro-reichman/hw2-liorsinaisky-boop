88b1176a07173064330ee4f1cc94ff10bc2468f7
Lior Sinaisky <Liorsinaisky@gmail.com>
2025-11-08 20:49:31 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,24 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	        String Word = args[0];
+          String word = Word.toUpperCase();
+          int reps = Integer.parseInt(args[1]);
+          int place=0;
+          String vowels = "AEFHILMNORSX";
+          int i = 0;
+          
+          for (place=0; place<Word.length();place++){
+            char c = word.charAt(place);
+            if(vowels.indexOf(c) == -1){
+              System.out.println("Give me a " + word.charAt(place) + ": " + word.charAt(place) + "!");
+            }
+            else{
+                System.out.println("Give me an " + word.charAt(place) + ": " + word.charAt(place) + "!");
+            }   
+          }
+          for (int index=0;index<reps;index++){
+            System.out.println(word + "!!!");
+          }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,20 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalMimutes = (hours*60)+minutes+minutesToAdd;
+        int totalHours = totalMimutes/60;
+        int newHours = totalHours%24;
+        int newMinutes = totalMimutes-(totalHours*60);
+        if (totalHours<10 && newMinutes<10){
+            System.out.println("0" + newHours + ":0" + newMinutes);
+        }
+        else if (newHours>=10 && newMinutes<10){
+           System.out.println(newHours + ":0" + newMinutes);
+        }
+        else{
+            System.out.println(newHours + ":" + newMinutes);
+        }
     }
 }
